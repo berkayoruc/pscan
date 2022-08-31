5dd8a1a1482dfa255380967eab901343c22bbb55
berkay <berkayoruc@hotmail.com>
2022-09-01 00:01:26 +0300

    Initial Commit

---
+++ PScan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PScan
+//
+//  Created by berkay on 1.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PScan/PScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  PScanApp.swift
+//  PScan
+//
+//  Created by berkay on 1.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct PScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
